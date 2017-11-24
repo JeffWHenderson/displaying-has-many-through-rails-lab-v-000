3b5bc453a74e68b1d06281b8ccdbc04edf5b6ccd
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-24 18:04:56 +0000

    automatically backed up by learn

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,12 +2,12 @@ class Appointment < ActiveRecord::Base
   belongs_to :doctor
   belongs_to :patient
 
-  def human_date
+  def human_date  # ////// 5 commented code does same thing the one line of code does at the end ////////
     # month = self.appointment_datetime.strftime('%B')
     # day = self.appointment_datetime.strftime('%d')
     # year = self.appointment_datetime.strftime('%Y')
     # time = self.appointment_datetime.strftime('%H:%M')
-    #"#{month} #{day} #{year}, at #{time}"
+    #"#{month} #{day}, #{year} at #{time}"
     self.appointment_datetime.strftime('%B %d, %Y at %H:%M')
   end
 end
