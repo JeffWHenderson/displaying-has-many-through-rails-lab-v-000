7f73c972ef43e61c7fd4217c748b8d0f8da6e33e
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-24 18:01:48 +0000

    automatically backed up by learn

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -4,11 +4,11 @@ class Appointment < ActiveRecord::Base
 
   # Appointments have appointment_datetime
   def human_date
-    month = self.appointment_datetime.strftime('%B')
-    day = self.appointment_datetime.strftime('%d')
-    year = self.appointment_datetime.strftime('%Y')
-    time = self.appointment_datetime.strftime('%H:%M')
-    self.appointment_datetime.strftime('%B %d %Y at %H:%M')
+    # month = self.appointment_datetime.strftime('%B')
+    # day = self.appointment_datetime.strftime('%d')
+    # year = self.appointment_datetime.strftime('%Y')
+    # time = self.appointment_datetime.strftime('%H:%M')
     #"#{month} #{day} #{year}, at #{time}"
+    self.appointment_datetime.strftime('%B %d %Y at %H:%M')
   end
 end
