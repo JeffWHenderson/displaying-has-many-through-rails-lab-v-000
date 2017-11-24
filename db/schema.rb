9ea2882629fc3eb17982f9d3b9eb149c13c50a5c
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-24 16:47:48 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171124164124) do
+ActiveRecord::Schema.define(version: 20171124164628) do
+
+  create_table "appointments", force: :cascade do |t|
+    t.integer  "doctor_id"
+    t.integer  "patient_id"
+    t.datetime "appointment_datetime"
+  end
 
   create_table "doctors", force: :cascade do |t|
     t.string   "name"
