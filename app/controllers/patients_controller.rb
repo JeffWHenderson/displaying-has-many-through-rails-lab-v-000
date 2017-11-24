8d832462dd20a745c8a15b415c57eb4488e0a5d0
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-24 16:56:41 +0000

    automatically backed up by learn

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,13 +1,12 @@
 class PatientsController < ApplicationController
   def show
-    "inside the patient show page"
   end
 
   def new
-    "inside the patient edit page"
   end
 
   def create
+    raise params.inspect
     "inside the patient create action"
   end
 end
