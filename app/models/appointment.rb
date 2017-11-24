b92fd5bb6ebc5ad10a02e16c134fc1c5884bc0b8
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-24 18:01:53 +0000

    automatically backed up by learn

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,7 +2,6 @@ class Appointment < ActiveRecord::Base
   belongs_to :doctor
   belongs_to :patient
 
-  # Appointments have appointment_datetime
   def human_date
     # month = self.appointment_datetime.strftime('%B')
     # day = self.appointment_datetime.strftime('%d')
